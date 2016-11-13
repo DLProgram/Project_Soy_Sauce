038322d1acd92c6fd5558846d0f196d9da474efa
Daniel Luan <DanielLuan425@gmail.com>
2016-11-12 22:06:39 -0500

    add get team function, update readme mysql table

--- get_match_data.php
+++ get_match_data.php
@@ -1,9 +1,26 @@
 <?php 
 include "connect.php";
+function get_team($conn, $color, $match_num){
+    $match_data = $conn->query("SELECT * FROM match_data WHERE match_num={$match_num}")->fetch_assoc();
+    if($color == "blue"){
+        $team1 = $match_data['blue1'];
+        $team2 = $match_data['blue2'];
+    }elseif($color == "red"){
+        $team1 = $match_data['red1'];
+        $team2 = $match_data['red2'];
+    }else{
+        $team1 = $match_data['blue1'];
+        $team2 = $match_data['blue2'];
+    }
+    $match_name = $match_data['match_name'];
+    return array('team1' => $team1, 'team2' => $team2, 'match_name' => $match_name);
+}
 
-$match_data = $conn->query("SELECT * FROM match_data WHERE match_num={$match_num}")->fetch_assoc();
-$team1 = $match_data['blue1'];
-$team2 = $match_data['blue2'];
+$match_data = get_team($conn, $_SESSION['color'], $match_num);
+
+$team1 = $match_data['team1'];
+$team2 = $match_data['team2'];
+
 $match_name = $match_data['match_name'];
 
 ?>
--- submit_data.php
+++ submit_data.php
@@ -1,49 +1,58 @@
 <?php
-function process_input($array){
-    $result = array();
-    $result['match_num'] = $array['match_num'];
+    function process_input($array){
+        $result = array();
+        $result['match_num'] = $array['match_num'];
 
-    $result['auto_star_enable1'] = isset($array["auto_star_enable1"]) ? True : False;
-    $result['auto_star_range1'] = isset($array["auto_star_range1"]) ? True : False;
-    $result['auto_cube_enable1'] = isset($array["auto_cube_enable1"]) ? True : False;
-    $result['auto_cube_range1'] = isset($array["auto_cube_range1"]) ? True : False;
-    $result['auto_lift1'] = isset($array["auto_lift1"]) ? True : False;
+        $result['auto_star_enable1'] = isset($array["auto_star_enable1"]) ? 1 : 0;
+        $result['auto_star_range1'] = isset($array["auto_star_range1"]) ? 1 : 0;
+        $result['auto_cube_enable1'] = isset($array["auto_cube_enable1"]) ? 1 : 0;
+        $result['auto_cube_range1'] = isset($array["auto_cube_range1"]) ? 1 : 0;
+        $result['auto_lift1'] = isset($array["auto_lift1"]) ? 1 : 0;
 
-    $result['auto_star_enable2'] = isset($array["auto_star_enable2"]) ? True : False;
-    $result['auto_star_range2'] = isset($array["auto_star_range2"]) ? True : False;
-    $result['auto_cube_enable2'] = isset($array["auto_cube_enable2"]) ? True : False;
-    $result['auto_cube_range2'] = isset($array["auto_cube_range2"]) ? True : False;
-    $result['auto_lift2'] = isset($array["auto_lift2"]) ? True : False;
+        $result['auto_star_enable2'] = isset($array["auto_star_enable2"]) ? 1 : 0;
+        $result['auto_star_range2'] = isset($array["auto_star_range2"]) ? 1 : 0;
+        $result['auto_cube_enable2'] = isset($array["auto_cube_enable2"]) ? 1 : 0;
+        $result['auto_cube_range2'] = isset($array["auto_cube_range2"]) ? 1 : 0;
+        $result['auto_lift2'] = isset($array["auto_lift2"]) ? 1 : 0;
 
-    $result['auto'] = (int) $array['auto'];
+        $result['auto'] = (int) $array['auto'];
 
-    $result['drive_star_enable1'] = isset($array["drive_star_enable1"]) ? True : False;
-    $result['drive_star_range1'] = isset($array["drive_star_range1"]) ? True : False;
-    $result['drive_cube_enable1'] = isset($array["drive_cube_enable1"]) ? True : False;
-    $result['drive_cube_range1'] = isset($array["drive_cube_range1"]) ? True : False;
-    $result['drive_lift1'] = isset($array["drive_lift1"]) ? True : False;
+        $result['drive_star_enable1'] = isset($array["drive_star_enable1"]) ? 1 : 0;
+        $result['drive_star_range1'] = isset($array["drive_star_range1"]) ? 1 : 0;
+        $result['drive_cube_enable1'] = isset($array["drive_cube_enable1"]) ? 1 : 0;
+        $result['drive_cube_range1'] = isset($array["drive_cube_range1"]) ? 1 : 0;
+        $result['drive_lift1'] = isset($array["drive_lift1"]) ? 1 : 0;
 
-    $result['drive_star_enable2'] = isset($array["drive_star_enable2"]) ? True : False;
-    $result['drive_star_range2'] = isset($array["drive_star_range2"]) ? True : False;
-    $result['drive_cube_enable2'] = isset($array["drive_cube_enable2"]) ? True : False;
-    $result['drive_cube_range2'] = isset($array["drive_cube_range2"]) ? True : False;
-    $result['drive_lift2'] = isset($array["drive_lift2"]) ? True : False;
+        $result['drive_star_enable2'] = isset($array["drive_star_enable2"]) ? 1 : 0;
+        $result['drive_star_range2'] = isset($array["drive_star_range2"]) ? 1 : 0;
+        $result['drive_cube_enable2'] = isset($array["drive_cube_enable2"]) ? 1 : 0;
+        $result['drive_cube_range2'] = isset($array["drive_cube_range2"]) ? 1 : 0;
+        $result['drive_lift2'] = isset($array["drive_lift2"]) ? 1 : 0;
 
-    $result['drive'] = (int) $array['drive'];
+        $result['drive'] = (int) $array['drive'];
 
-    return $result;
-}
-if(isset($_POST["submit"])){
-    if($_POST['submit'] == "Submit"){
-        $result = process_input($_POST);
-        $match_num = $result['match_num'];
-        $match_num = $match_num + 1;
-    }elseif($_POST['submit'] == "Update"){
-        $match_num = $_POST['match_num'];
+        return $result;
     }
-}
+    function submit_data($conn, $data){
+        $q = "INSERT INTO `submitted_result` (`match_num`, `color`, `auto_star_enable1`, `auto_star_range1`, `auto_cube_enable1`, `auto_cube_range1`, `auto_lift1`, `auto_star_enable2`, `auto_star_range2`, `auto_cube_enable2`, `auto_cube_range2`, `auto_lift2`, `auto`, `drive_star_enable1`, `drive_star_range1`, `drive_cube_enable1`, `drive_cube_range1`, `drive_lift1`, `drive_star_enable2`, `drive_star_range2`, `drive_cube_enable2`, `drive_cube_range2`, `drive_lift2`, `drive`) VALUES ('{$data['match_num']}', '{$_SESSION['color']}', '{$data['auto_star_enable1']}', '{$data['auto_star_range1']}', '{$data['auto_cube_enable1']}', '{$data['auto_cube_range1']}', '{$data['auto_lift1']}', '{$data['auto_star_enable2']}', '{$data['auto_star_range2']}', '{$data['auto_cube_enable2']}', '{$data['auto_cube_range2']}', '{$data['auto_lift2']}', '{$data['auto']}', '{$data['drive_star_enable1']}', '{$data['drive_star_range1']}', '{$data['drive_cube_enable1']}', '{$data['drive_cube_range1']}', '{$data['drive_lift1']}', '{$data['drive_star_enable2']}', '{$data['drive_star_range2']}', '{$data['drive_cube_enable2']}', '{$data['drive_cube_range2']}', '{$data['drive_lift2']}', '{$data['drive']}')";
+        if (!$conn->query($q)){
+            die(mysqli_error($conn));
+        }else{
+            return True;
+        }
+    }
+    if(isset($_POST["submit"])){
+        if($_POST['submit'] == "Submit"){
+            $data = process_input($_POST);
+            $match_num = $data['match_num'];
+            $match_num = $match_num + 1;
+            submit_data($conn, $data);
+        }elseif($_POST['submit'] == "Update"){
+            $match_num = $_POST['match_num'];
+        }
+    }
 
-if (!isset($match_num)){
-    $match_num = 1;
-}
+    if (!isset($match_num)){
+        $match_num = 1;
+    }
 ?>
