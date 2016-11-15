b9ef522c3102a6b71ba328189e0c83a393a77548
Daniel Luan <DanielLuan425@gmail.com>
2016-11-15 15:26:00 -0500

    fix check data page

--- check_data.php
+++ check_data.php
@@ -3,25 +3,15 @@ include 'lock.php';
 include 'connect.php';
 
 function get_unchecked_data($conn){
-    $match_nums = $conn->query("SELECT DISTINCT(match_num) FROM submitted_result")->fetch_all();
-    $unchecked_macthes = [];
-    foreach ($match_nums as $target_match) {
-        $red_data = $conn->query("SELECT * FROM submitted_result WHERE match_num={$target_match[0]} AND color='red'")->fetch_assoc();
-        $blue_data = $conn->query("SELECT * FROM submitted_result WHERE match_num={$target_match[0]} AND color='blue'")->fetch_assoc();
-        $unchecked_macthes[$target_match[0]] = array('red' => $red_data, 'blue' => $blue_data);
-    }
-    return $unchecked_macthes;
+    $submitted_matches = $conn->query("SELECT * FROM submitted_result")->fetch_all(MYSQLI_ASSOC);
+    return $submitted_matches;
 }
 
-function process_checked_data($conn, $match_num, $score){
-    $red_data = $conn->query("SELECT * FROM submitted_result WHERE match_num={$match_num} AND color='red'")->fetch_assoc();
-    $blue_data = $conn->query("SELECT * FROM submitted_result WHERE match_num={$match_num} AND color='blue'")->fetch_assoc();
-    $red1 = array('match_num' => $match_num, 'team' => $red_data['team1'], 'auto_star_enable' => $red_data['auto_star_enable1'], 'auto_star_range' => $red_data['auto_star_range1'],'auto_cube_enable' => $red_data['auto_cube_enable1'],'auto_cube_range' => $red_data['auto_cube_range1'],'auto_lift' => $red_data['auto_lift1'],'auto' => $red_data['auto'], 'drive_star_enable' => $red_data['drive_star_enable1'], 'drive_star_range' => $red_data['drive_star_range1'],'drive_cube_enable' => $red_data['drive_cube_enable1'],'drive_cube_range' => $red_data['drive_cube_range1'],'drive_lift' => $red_data['drive_lift1'], 'drive' => ($red_data['drive'] * $score / 100));
-    $red2 = array('match_num' => $match_num, 'team' => $red_data['team2'], 'auto_star_enable' => $red_data['auto_star_enable2'], 'auto_star_range' => $red_data['auto_star_range2'],'auto_cube_enable' => $red_data['auto_cube_enable2'],'auto_cube_range' => $red_data['auto_cube_range2'],'auto_lift' => $red_data['auto_lift2'],'auto' => $red_data['auto'], 'drive_star_enable' => $red_data['drive_star_enable2'], 'drive_star_range' => $red_data['drive_star_range2'],'drive_cube_enable' => $red_data['drive_cube_enable2'],'drive_cube_range' => $red_data['drive_cube_range2'],'drive_lift' => $red_data['drive_lift2'], 'drive'=> (100 - $red_data['drive']) * $score / 100);
-    $blue1 = array('match_num' => $match_num, 'team' => $blue_data['team1'], 'auto_star_enable' => $blue_data['auto_star_enable1'], 'auto_star_range' => $blue_data['auto_star_range1'],'auto_cube_enable' => $blue_data['auto_cube_enable1'],'auto_cube_range' => $blue_data['auto_cube_range1'],'auto_lift' => $blue_data['auto_lift1'],'auto' => $blue_data['auto'], 'drive_star_enable' => $blue_data['drive_star_enable1'], 'drive_star_range' => $blue_data['drive_star_range1'],'drive_cube_enable' => $blue_data['drive_cube_enable1'],'drive_cube_range' => $blue_data['drive_cube_range1'],'drive_lift' => $blue_data['drive_lift1'], 'drive' => ($blue_data['drive'] * $score / 100));
-    $blue2 = array('match_num' => $match_num, 'team' => $blue_data['team2'], 'auto_star_enable' => $blue_data['auto_star_enable2'], 'auto_star_range' => $blue_data['auto_star_range2'],'auto_cube_enable' => $blue_data['auto_cube_enable2'],'auto_cube_range' => $blue_data['auto_cube_range2'],'auto_lift' => $blue_data['auto_lift2'],'auto' => $blue_data['auto'], 'drive_star_enable' => $blue_data['drive_star_enable2'], 'drive_star_range' => $blue_data['drive_star_range2'],'drive_cube_enable' => $blue_data['drive_cube_enable2'],'drive_cube_range' => $blue_data['drive_cube_range2'],'drive_lift' => $blue_data['drive_lift2'], 'drive' => (100 - $blue_data['drive']) * $score / 100);
-    delete_match_data($conn, $match_num);
-    return array($red1, $red2, $blue1, $blue2);
+function process_checked_data($conn, $id, $score){
+    $match_data = $conn->query("SELECT * FROM submitted_result WHERE id={$id}")->fetch_assoc();
+    $team1 = array('match_num' => $match_data['match_num'], 'team' => $match_data['team1'],'auto_star_enable' => $match_data['auto_star_enable1'], 'auto_star_range' => $match_data['auto_star_range1'],'auto_cube_enable' => $match_data['auto_cube_enable1'],'auto_cube_range' => $match_data['auto_cube_range1'],'auto_lift' => $match_data['auto_lift1'],'auto' => ($match_data['auto'] * $score / 100), 'drive_star_enable' => $match_data['drive_star_enable1'], 'drive_star_range' => $match_data['drive_star_range1'],'drive_cube_enable' => $match_data['drive_cube_enable1'],'drive_cube_range' => $match_data['drive_cube_range1'],'drive_lift' => $match_data['drive_lift1'], 'drive' => ($match_data['drive'] * $score / 100));
+    $team2 = array('match_num' => $match_data['match_num'], 'team' => $match_data['team2'],'auto_star_enable' => $match_data['auto_star_enable2'], 'auto_star_range' => $match_data['auto_star_range2'],'auto_cube_enable' => $match_data['auto_cube_enable2'],'auto_cube_range' => $match_data['auto_cube_range2'],'auto_lift' => $match_data['auto_lift2'],'auto' => ($match_data['auto'] * $score / 100), 'drive_star_enable' => $match_data['drive_star_enable2'], 'drive_star_range' => $match_data['drive_star_range2'],'drive_cube_enable' => $match_data['drive_cube_enable2'],'drive_cube_range' => $match_data['drive_cube_range2'],'drive_lift' => $match_data['drive_lift2'], 'drive' => ($match_data['drive'] * $score / 100));
+    return array($team1, $team2);
 }
 
 function submit_checked_data($conn, $checked_data){
@@ -33,8 +23,8 @@ function submit_checked_data($conn, $checked_data){
     }
 }
 
-function delete_match_data($conn, $match_num){
-    $dresult = $conn->query("DELETE FROM `submitted_result` WHERE match_num={$match_num}");
+function delete_match_data($conn, $id){
+    $dresult = $conn->query("DELETE FROM `submitted_result` WHERE id={$id}");
     if (!$dresult){
         die(mysqli_error($conn));
     }
@@ -45,15 +35,16 @@ if(!empty($_GET)){
         if(empty($_GET['score'])){
             $error = "Please enter a score!!";
         }else{
-            submit_checked_data($conn, process_checked_data($conn, $_GET['match_num'], $_GET['score']));
+            submit_checked_data($conn, process_checked_data($conn, $_GET['id'], $_GET['score']));
+            delete_match_data($conn, $_GET['id']);
         }
     }elseif($_GET['submit'] == "Delete"){
-        delete_match_data($conn, $_GET['match_num']);
+        delete_match_data($conn, $_GET['id']);
     }
     
 }
 
-$unchecked_macthes = get_unchecked_data($conn);
+$unchecked_matches = get_unchecked_data($conn);
 
 ?>
 <!DOCTYPE html>
@@ -92,26 +83,45 @@ $unchecked_macthes = get_unchecked_data($conn);
     <div class="row">
         <table>
             <tr>
+                <th width="5%">ID</th>
                 <th width="10%">Match #</th>
                 <th width="20%">Teams</th>
                 <th>Score</th>
                 <th width="20%">Submit</th>
             </tr>
             <?php
-            foreach ($unchecked_macthes as $match_num => $teams) {
-                echo"
-                <tr>
-                    <form>
-                        <td align='right'>{$match_num}<input type='hidden' name='match_num' value={$match_num}></td>
-                        <td align='center'><span class='alert label'>{$teams['red']['team1']} {$teams['red']['team2']}</span><span class='info label'>{$teams['blue']['team1']} {$teams['blue']['team2']}</span></td>
-                        <td><input type='number' name='score'></td>
-                        <td align='right'>
-                            <input type='submit' class='success button' value='Submit' name='submit'>
-                            <input type='submit' class='alert button' value='Delete' name='submit'>
-                        </td>
-                    </form>
-                <tr>
-                ";
+            foreach ($unchecked_matches as $match) {
+                if ($match['color'] == 'red'){
+                    echo"
+                    <tr>
+                        <form>
+                            <td>{$match['id']}<input type='hidden' name='id' value={$match['id']}></td>
+                            <td>{$match['match_num']}</td>
+                            <td><span class='alert label'>{$match['team1']} {$match['team2']}</span></td>
+                            <td><input type='number' name='score'></td>
+                            <td>
+                                <input type='submit' class='success button' value='Submit' name='submit'>
+                                <input type='submit' class='alert button' value='Delete' name='submit'>
+                            </td>
+                        </form>
+                    <tr>
+                    ";
+                }elseif ($match['color'] == 'blue'){
+                    echo"
+                    <tr>
+                        <form>
+                            <td>{$match['id']}<input type='hidden' name='id' value={$match['id']}></td>
+                            <td>{$match['match_num']}</td>
+                            <td><span class='info label'>{$match['team1']} {$match['team2']}</span></td>
+                            <td><input type='number' name='score'></td>
+                            <td>
+                                <input type='submit' class='success button' value='Submit' name='submit'>
+                                <input type='submit' class='alert button' value='Delete' name='submit'>
+                            </td>
+                        </form>
+                    <tr>
+                    ";
+                }
             }
             ?>
         </table>
